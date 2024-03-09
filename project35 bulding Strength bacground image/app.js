2cdf44d254feba317cd5cba6fbb258fc88baac30
deepak <deepaklodha532@gmail.com>
2024-03-09 15:38:47 +0530

    password 35

---
+++ project35 bulding Strength bacground image/app.js
@@ -0,0 +1,13 @@
+const password  =  document.getElementById("password");
+const bg=  document.querySelector(".background-image");
+
+console.log(password);
+console.log(bg);
+
+password.addEventListener('input', e=>{
+     const input =  e.target.value;
+     const length =  input.length ; 
+     const blurness  =  20 - length *2; 
+     bg.style.filter =  `blur(${blurness}px)` ;
+
+})
