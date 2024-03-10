701aecb2c1d7f9fd9ce656ec950d2f6b04431e50
deepak <deepaklodha532@gmail.com>
2024-03-10 09:50:37 +0530

    Read More

---
+++ project39 Read more read list/app.js
@@ -0,0 +1,8 @@
+let contentContainer2  =  document.querySelector(".content-container-2");
+const btn = document.querySelector(".btn");
+
+
+
+btn.addEventListener('click', ()=>{
+  contentContainer2.classList .toggle("toggle"); 
+})
